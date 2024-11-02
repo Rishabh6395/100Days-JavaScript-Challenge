897786662127fca7f77abfb9854db7a905b3f79b
Rishabh <rishabhbhatt956@gmail.com>
2024-11-02 06:08:50 +0530

    Function findMedian(arr) that takes an array of numbers as input and returns the median value.

---
+++ 22/index.js
@@ -0,0 +1,27 @@
+// Write a javascript function findMedian(arr) that takes an array of numbers as input and returns the median value. If the array has an even number of elements, return the average of the two middle values.
+
+// For eg. the median 3, 3, 5, 9, 15 is 5. If there is an even number of observations, then there is no single middle value; the median is then usually defined to be the mean of the two middle values: so the midan of 3, 5, 7, 9 is (5+7)/2 = 6.
+
+// todo tips:
+// Sort the array in ascending order.
+// If the array has an odd num of elements, the median is middle element.
+// If the array has an even number of elements, the median is the average of the two middle elements.
+
+const findMedian = (arr) =>{
+    arr.sort((a,b) => a-b)
+    
+    let length = arr.length
+    let mid = Math.floor(length/2)
+
+    if(length % 2 ===0){
+        return (arr[mid] + arr[mid - 1]) /2
+    }
+    else{
+        return arr[mid]
+    }
+
+}
+
+console.log(findMedian([5, 3, 9, 1, 7])) // o/p: 5
+console.log(findMedian([2, 4, 6, 8])) // o/p: 5
+console.log(findMedian([1, 3, 5, 7, 9, 11])) // o/p: 6
