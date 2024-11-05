b56bd86ba95b8c5ef5a22934ba8db9bdb03fed3f
Rishabh <rishabhbhatt956@gmail.com>
2024-11-05 05:59:32 +0530

    calculate the factorial of a number. (Using Recursion)

---
+++ 25/index.js
@@ -0,0 +1,13 @@
+// Write a function to calculate the factorial of a number. (Using Recursion)
+
+const factorial = (num) =>{
+    if(num === 1){
+        return 1
+    }
+    else{
+        return num * factorial(num-1)
+    }
+}
+
+
+console.log(factorial(5)) // op: 120
