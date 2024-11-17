83b173e03522a3bba9faba46e1389952d4fe1df4
Rishabh <rishabhbhatt956@gmail.com>
2024-11-17 07:18:33 +0530

    Simple Interest Calculator

---
+++ 37/index.js
@@ -0,0 +1,9 @@
+// Simple Interest Calculator
+
+// Write a function calculateSimpleInterest that calculates the simple interest given the principal amount, rate of interest per annum, and time in years.
+
+const calculateSimplInterest = (p, r, t) => {
+    return si = p*r*t / 100;
+}
+
+console.log(calculateSimplInterest(1000, 5, 3)) // Output: 150
