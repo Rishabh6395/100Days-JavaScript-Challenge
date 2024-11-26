ed0be0160009c40418af42d34c8b85acf65700a9
Rishabh <rishabhbhatt956@gmail.com>
2024-11-27 04:50:47 +0530

    Extract Numbers from a String

--- 47/index.js
+++ 47/index.js
@@ -2,3 +2,12 @@
 
 // Write a regular expression to extract all numbers from a given string.
 
+const extractNumbers = (num) =>{
+    let regex =  /\d+/g
+    return num.match(regex)
+}
+
+console.log(extractNumbers("absc12432dsf213")) // Output: ["12432", "213"]
+console.log(extractNumbers("no numbers here")) // Output: null
+console.log(extractNumbers("1a2b3c4d")) // Output: ["1","2","3","4"]
+console.log(extractNumbers("")) // Output: null
