48140ebd77ff9cce0b2f364b7e33beff11c2b7f3
Rishabh <rishabhbhatt956@gmail.com>
2024-11-30 06:14:39 +0530

    Example of Hoisting & Temoral Dead Zone

---
+++ Javascript Qs by Piyush/01.js
@@ -0,0 +1,21 @@
+// COMMENT ONE OF THESE QUESTIONS BEFORE RUNNING THE CODE!!
+
+
+// Q1 =:
+// Hositing (because of var keyword)
+console.log("Value of Age is", age)
+
+var age = 20;
+
+console.log("Value of age is ", age)
+
+
+// Q1.1 =:
+// Temporal Dead Zone (because of let keyword)
+age = 100;
+
+console.log("Value of Age is", age)
+
+let age = 20;
+
+console.log("Value of Age is", age)
