accdc43e5d9831df6f51e80ea251e348a41900bb
Rishabh <rishabhbhatt956@gmail.com>
2024-12-18 07:20:19 +0530

    How would you check if a number is an integer?

---
+++ Questions from Sheryians/04Four.js
@@ -0,0 +1,10 @@
+// How would you check if a number is an integer? (Without using prebuilt function like isInteger)
+
+var a = 12
+
+if(a%1 === 0 ){
+    console.log("Integer")
+}
+else{
+    console.log("Not a Integer")
+}
