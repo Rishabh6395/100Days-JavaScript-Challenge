09d4c4c29f9f37cea908bdcab378a26e8a846c4a
Rishabh <rishabhbhatt956@gmail.com>
2025-02-07 04:26:38 +0530

    Add basic console logging examples for name, calculations, and variable updates

---
+++ More Questions by sheryians/Basic Console Usage/01.js
@@ -0,0 +1,18 @@
+// Log Your name and favourite hobby to the console
+console.log("Rishabh")
+
+// Perform and log the result of 45 * 2 - 10
+console.log(45*2-10)
+
+// Use console.log() to display the current year.
+const dt = new Date()
+console.log(dt.getFullYear())
+
+// Create two variables by logging it before and after updating.
+let a = 10;
+let b = 20;
+
+console.log("Before updaing the value of a and b is", a ,",", b)
+a = 30;
+b = 40;
+console.log("After updaing the value of a and b is", a ,",", b)
