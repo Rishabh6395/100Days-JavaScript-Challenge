92aa14111f98a70797d5bae90a1986ac48fcf6e1
Rishabh <rishabhbhatt956@gmail.com>
2025-02-09 08:35:13 +0530

    Add examples for variable declaration, reassignment, and type checking

---
+++ More Questions by sheryians/Variable & DataTypes/01.js
@@ -0,0 +1,19 @@
+// Declare a variable using let and log its value.
+// let a = 12;
+// console.log(a)
+
+// Create a constant to store the value of PI and log it.
+// const pi = Math.PI
+// console.log(pi)
+
+// Reassign a value to a variable declared dwith let and log the result.
+// let a = 12;
+// a = 32;
+// console.log(a)
+
+// Check the tyep of null and log it.
+// console.log(typeof null)
+
+// Create a variable with a number as a string (e.g., "25") and log its type.
+// let a = "25"
+// console.log(typeof a)
