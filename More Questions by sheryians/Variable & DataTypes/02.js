3abc31e6c7f1dac891b7b6a4da2b2d99b0bbf554
Rishabh <rishabhbhatt956@gmail.com>
2025-02-10 04:32:36 +0530

    Add examples for type checking with boolean and undefined variables

---
+++ More Questions by sheryians/Variable & DataTypes/02.js
@@ -0,0 +1,15 @@
+// Use typeof to check the type of boolean variable
+// let a = true;
+// console.log(typeof a)
+
+// Create three variables of types string, number and boolean and log their values.
+// let a = "Hey", b = 12, c = true;
+// console.log(a,b,c)
+
+// Declare a variable without assigning a value. Log its type.
+// let a;
+// console.log(typeof a)
+
+// Create a variable with undefined and log its type
+let a = undefined;
+console.log(typeof a)
