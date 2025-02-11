75411a799184707326603ee2479318e8f874dcac
Rishabh <rishabhbhatt956@gmail.com>
2025-02-12 04:12:24 +0530

    Add examples for variable initialization, loops, and string iteration

---
+++ More Questions by sheryians/Variable & DataTypes/03.js
@@ -0,0 +1,27 @@
+// Create a variable with undefined and log its type.
+// const aa = [1,2,3,4]
+// // aa = 12 // Error reassign is not possible
+// aa.pop()
+
+
+// Loops:
+// Write a for loop to print numbers from 1 to 50.
+// for (let i = 0; i <= 50; i++) {
+//     console.log(i)
+// }
+
+// Use a while loop to sum the numbers from 1 to 10.
+// let i =1;
+// let s = 0
+// while(i<=10){
+//     s = s + i;
+//     console.log(s)
+//     i++
+// }
+
+// Create a for...of loop to loop to log each character of the string "JavaScript"
+
+// let str = "JavaScript"
+// for(let newVar of str){
+//     console.log(newVar)
+// }
