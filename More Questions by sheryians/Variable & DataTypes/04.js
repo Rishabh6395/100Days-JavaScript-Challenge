eef0f6d1c2da9af026825496d11bd4fa6cf78e3c
Rishabh <rishabhbhatt956@gmail.com>
2025-02-15 11:27:17 +0530

    Add examples for for loop, do..while loop, and factorial calculation

---
+++ More Questions by sheryians/Variable & DataTypes/04.js
@@ -0,0 +1,25 @@
+// Write a for loop that skips even numbers between 1 and 20;
+
+// for(var i = 1; i <= 20; i++){
+//     if(i%2 === 0){}
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// Use a do..while loop 
+
+// var a = 5;
+// do{ 
+//     console.log(a)
+//     a--
+// }
+// while(a>0){
+
+// }
+
+// Create a for loop that calculates the factorial of 5.
+// let fact = 1
+// for(var i = 5; i>0; i--){
+//     fact * i;
+// }
