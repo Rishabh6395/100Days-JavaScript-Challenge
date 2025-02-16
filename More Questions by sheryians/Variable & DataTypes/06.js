c503f2f4194f7446c9d6eef7f9c7dd07424ea5c7
Rishabh <rishabhbhatt956@gmail.com>
2025-02-17 05:09:44 +0530

    Add examples for while loop and for...in loop iteration

---
+++ More Questions by sheryians/Variable & DataTypes/06.js
@@ -0,0 +1,18 @@
+// Write a while loop that logs numbers  from 1 to 100 divisible by 5.
+
+// let num = 1;
+// while(num<=100){
+//     if(num%5===0) console.log(num)
+//     num++
+// }
+
+// Use a for...in loop to iterate over an object and log its keys.
+var obj ={
+    name: "a",
+    email: "a@emaple.com",
+    age: 26
+}
+
+for(let key in obj){
+    console.log(key)
+}
