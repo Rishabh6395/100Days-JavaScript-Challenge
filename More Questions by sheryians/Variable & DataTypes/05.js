8b8cc0bd1cb868fe1c64c2466cc80b689918fd05
Rishabh <rishabhbhatt956@gmail.com>
2025-02-16 09:36:53 +0530

    Add examples for nested loops and array reversal

---
+++ More Questions by sheryians/Variable & DataTypes/05.js
@@ -0,0 +1,22 @@
+// Write a nested loop to print a 3x3 grid of numbers.
+// var hold = 1;
+// for(var i =1; i<4; i++){
+//     var str = ""
+//     for(var j =1; j<4; j++){
+//         str += `${hold} `;
+//         hold++;
+//     }
+//     console.log(str)
+// }
+
+// Use a loop to reverse an array [1,2,3,4].
+// var arr = [1,2,3,4,5]
+// // var re = arr.reverse()
+// // console.log(re)
+// for(var i =0; i<Math.floor(arr.length/2); i++){
+//     var temp = arr[i]
+//     arr[i] = arr[arr.length - 1 - i];
+//     arr[arr.length-i -1] = temp;
+// }
+// console.log(arr)
+
