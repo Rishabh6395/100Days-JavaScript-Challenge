1d8fde1091daf549e61642ced6e0521da553d3f9
Rishabh <rishabhbhatt956@gmail.com>
2025-02-18 04:49:46 +0530

    Add examples for array manipulation including logging elements, unshifting, and popping

---
+++ More Questions by sheryians/Variable & DataTypes/07.js
@@ -0,0 +1,23 @@
+// ARRAY
+// Create an arrya of your top 5 favorite movies and log it.
+// var arr = ["a", "b", "c", "d", "e"]
+// arr.forEach(function(val){
+//     console.log(val)
+// })
+
+// Find and log the second element of an array.
+// var arr = [1,2,3,4,5,6]
+// console.log(arr[1])
+
+// Add two new elements to the start of an array using .unshift().
+// var arr = [1,2,3,4,5,6]
+
+// arr.unshift(0)
+// arr.unshift(-1)
+// console.log(arr)
+
+
+// Remove the last element of an array and log the updated array.
+var arr = [1,2,3,4,5,6]
+arr.pop()
+console.log(arr)
