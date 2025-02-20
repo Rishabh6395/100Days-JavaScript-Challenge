15bdea86df883ba2598593bec4773b060c4b016d
Rishabh <rishabhbhatt956@gmail.com>
2025-02-21 04:27:40 +0530

    Add examples for array methods: slice, indexOf, and includes

---
+++ More Questions by sheryians/Variable & DataTypes/08.js
@@ -0,0 +1,15 @@
+// Use .slice() to extract the first three element of an array  
+// var arr = [1,2,3,4,5,6,7]
+// console.log(arr.splice(0,3))
+
+// Find the index of a specific element in an array using .indexOf()
+// var arr = [1,2,3,4,5,6,7]
+// console.log(arr.indexOf(4))
+
+// Check if a value exists in an array using .includes()
+var arr = [1,2,3,4,5,6,7]
+console.log(arr.includes(12))
+
+
+
+
