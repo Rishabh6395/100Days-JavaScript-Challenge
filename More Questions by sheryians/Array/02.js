8e4aaabaecd69efc5585b75a9c5f1cc7df1e0414
Rishabh <rishabhbhatt956@gmail.com>
2025-02-23 08:56:13 +0530

    Add examples for array copying and even/odd number checking functions

---
+++ More Questions by sheryians/Array/02.js
@@ -0,0 +1,14 @@
+// Write a program that creates a copy of an array without mutating the original.
+// var arr = [11, 3, 53, 5, 34]
+// var arr2 = []
+
+// arr.forEach(function(value){
+//     arr2.push(value)
+// })
+
+// arr2.pop()
+// console.log(arr2)
+
+
+
+
---
+++ More Questions by sheryians/Functions/01.js
@@ -0,0 +1,15 @@
+// Write a function to check if a number is even or odd.
+
+// function evodd(value){
+//     if(value%2 === 0) return "Even"
+//     else return "odd"
+// }
+// console.log(evodd(12))
+
+
+// Create a function to calculate the area of a circle with a given radius.
+
+// function area(r){
+//     return Math.round(Math.PI * r * r);
+// }
+// console.log(area(12))
