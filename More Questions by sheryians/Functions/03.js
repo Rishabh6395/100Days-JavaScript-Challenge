05088186f9f5cfc732c73d1d42f5e30c75b8ac43
Rishabh <rishabhbhatt956@gmail.com>
2025-02-25 04:24:48 +0530

    Add functions to find the maximum of two numbers and calculate factorial

---
+++ More Questions by sheryians/Functions/03.js
@@ -0,0 +1,25 @@
+// Write a function to find the maximum of two numbers.
+
+// function max(a,b){
+//     if(a>b) return a;
+//     else if (a<b) return b;
+//     else return "equals"
+// }
+
+// console.log(max(12,6))
+
+
+// Write a function to find the maximum of two numbers.
+
+// function getFact(num){
+//     let fct =1;
+//     for(var i = 1; i<=num; i++){
+//         fct = fct * i;
+//     }
+//     return fct;
+// }
+
+// console.log(getFact(5))
+
+
+
---
+++ More Questions by sheryians/Functions/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+n max(a,b){
+    if(a>b) return a;
+    else if (a<b) return b;
+    else return "equals"
+}
+
+console.
